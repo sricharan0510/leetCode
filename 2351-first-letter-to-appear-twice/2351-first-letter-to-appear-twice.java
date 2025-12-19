9a1c55957232ed0bd664d9451256c42f8dd6d19a
Sricharan Illandula <22mh1a4223@acoe.edu.in>
2025-12-19 09:50:22 +0530

    Time: 0 ms (100%), Space: 42.9 MB (35.95%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,10 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c:s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) +1);
+            if(map.get(c) == 2) return c;
+        }
+        return ' ';
+    }
+}
