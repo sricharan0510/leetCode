31b8dd800ea8aee210afe020b9eb17943a7f8862
Sricharan Illandula <22mh1a4223@acoe.edu.in>
2026-01-18 11:36:23 +0530

    Time: 6 ms (13.42%), Space: 46.9 MB (6.19%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.java
@@ -0,0 +1,26 @@
+// class Solution {
+//     public String reversePrefix(String s, int k) {
+//         StringBuilder sb = new StringBuilder();
+//         for(int i = k - 1; i >= 0; i--) {
+//             sb.append(s.charAt(i));
+//         }
+//         for(int i = k; i < s.length(); i++) {
+//             sb.append(s.charAt(i));
+//         }
+//         return sb.toString();
+//     }
+// }
+
+
+class Solution {
+    public String reversePrefix(String s, int k) {
+        String str = "";
+        for(int i = k - 1; i >= 0; i--) {
+            str += s.charAt(i);
+        }
+        for(int i = k; i < s.length(); i++) {
+            str += s.charAt(i);
+        }
+        return str;
+    }
+}
